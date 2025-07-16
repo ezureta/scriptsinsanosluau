6744e3bc4632188107ab4254e96730ab5e9f563f
ezureta <59396610+ezureta@users.noreply.github.com>
2025-07-16 14:04:38 -0600

    Update as1d2.lua

--- asd2.lua
+++ asd2.lua
@@ -1,5 +1,5 @@
 local player = game.Players.LocalPlayer
---3
+--4
 -- Función para encontrar la granja del jugador
 local function encontrarMiGranja()
     local raiz = workspace:FindFirstChild("Farm")
